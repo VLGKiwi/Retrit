518a05af64df35cd460bbc9caf8db368b5de5367
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-09-27 17:48:25 +0300

    feat: Intro/end Header

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,10 @@
+import { Header } from './Header/Header';
+import styles from './style.module.css'
+
+export const Intro = () => {
+	return (
+		<section className={styles.intro}>
+			<Header />
+		</section>
+	)
+}
