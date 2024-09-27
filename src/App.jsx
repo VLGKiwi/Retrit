1fd85c5995c7e8dcbfe2ada2475440d903e2fa0a
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-09-27 19:31:09 +0300

    feat: About/end section About

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { About } from './components/About/About';
 import { Intro } from './components/Intro/Intro';
 
 function App() {
   return (
     <>
       <Intro />
+      <About />
     </>
   )
 }
