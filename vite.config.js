081b0e8228d8c14cf4dfb4c9a28bf25bb8eb3444
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-10-02 20:26:25 +0300

    error2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['gsap', 'gsap/Observer', '@gsap/react']
+      external: ['gsap', 'gsap/*', '@gsap/react']
     }
   }
 })
