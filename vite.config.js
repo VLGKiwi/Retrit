781ba7f549763968ed43da1842208d2c18f01737
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-10-02 20:33:39 +0300

    error5

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { builtinModules } from 'module'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: id => {
-        // Если модуль начинается с одного из этих путей, он будет считаться внешним
-        if (id.includes('node_modules')) return true;
-        if (builtinModules.includes(id)) return true;
-        return false;
-      }
+      external: ['gsap'],
     }
+  },
+  optimizeDeps: {
+    include: ['gsap']
   }
 })
